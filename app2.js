d581dcdd68a4d4d3b21af888f4248ab6a63b3037
Winbylll <winbylll@gmail.com>
2022-08-14 21:37:11 +0200

    qw

---
+++ app2.js
@@ -0,0 +1,45 @@
+// // function count(a, b, operation) {
+// //     let result;
+// //     switch (operation) {
+// //         case ("+"):
+// //             outcome = a + b;
+// //             break;
+       
+// //         case ("*"):
+// //             outcome = a * b;
+// //             break;
+// //         case ("-"):
+// //             outcome = a - b;
+// //  }
+// //     return result;
+// // }
+// // console.log(calculate ('+', 12, 20))
+// // console.log(calculate ('*', 5, 15))
+// // console.log(calculate ('-', 11, 25))
+
+
+
+// function calculate (math, a , b) {
+//    let result
+//    switch (math) {
+//       case ('+'):
+//          result = a + b ;
+//          break;
+//       case ('-'):
+//          result = a - b ;
+//          break;
+//       case ('*'):
+//          result = a * b ;
+//    }
+//    return result
+//    }
+
+
+// console.log(calculate ('+', 12, 15))
+// console.log(calculate ('-', 121, 105))
+// console.log(calculate ('*', 11, 12))
+const add = function (a, b, c) {
+   return a + b + c
+}
+const result = add(6, 7, 8);
+console.log(result);
